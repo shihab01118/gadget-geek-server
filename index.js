8cd68314a006e4359a1a796856e351e94cadff1f
shihab01118 <shahed.shihab.0@gmail.com>
2023-10-19 02:17:23 +0600

    post api added for adding product to database

--- index.js
+++ index.js
@@ -27,9 +27,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const productsCollection = client.db("productsDB").collection("products");
+
+    app.post("/products", async(req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
