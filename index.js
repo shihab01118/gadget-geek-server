958ae87b007d60578d0d75e8c3b9370daa1e6c16
shihab01118 <shahed.shihab.0@gmail.com>
2023-10-19 20:10:33 +0600

    .env and vercel.json file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
@@ -9,11 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// shahedshihab0
-// vkFod6Y0k4kckwKa
-
 const uri =
-  "mongodb+srv://shahedshihab0:vkFod6Y0k4kckwKa@cluster0.tc6ru5z.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tc6ru5z.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
