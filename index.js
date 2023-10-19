c9ed0b4d5d71e52d6f22f42faef520abece19763
shihab01118 <shahed.shihab.0@gmail.com>
2023-10-19 14:20:18 +0600

    get method added for loading single product data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -40,6 +40,13 @@ async function run() {
       const brandName = req.params.brand;
       const query = {brand: brandName};
       const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get("/product/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
       res.send(result);
     })
 
